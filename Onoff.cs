b4f431fee0fe6df1fabe789a24de468ba77c5699
KimKunWoo0407 <52202558+KimKunWoo0407@users.noreply.github.com>
2019-12-02 02:45:46 +0900

    유니티데이터랑 파일입니다(혹시몰라서)
    3D스캐닝 파일이랑, 유니티 씬, 코드(불완전)입니다.

---
+++ Onoff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onoff : MonoBehaviour
+{
+    public static bool onoff = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onoff == true)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            onoff = false;
+        }
+            
+        
+    }
+
+   
+}
---
+++ Touch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            UnityEngine.Touch touch = Input.GetTouch(0);
+            Vector3 touchPos;
+            Ray ray;
+            RaycastHit hit;
+            switch(touch.phase)
+            {
+                case TouchPhase.Began:
+                    Vector3 touchPosToVector3 = new Vector3(touch.position.x, touch.position.y, 100);
+                    touchPos = Camera.main.ScreenToWorldPoint(touchPosToVector3);
+                    ray = Camera.main.ScreenPointToRay(touchPosToVector3);
+                    break;
+                case TouchPhase.Ended:
+                    Onoff.onoff = true; break;
+                default: break;
+            }
+        }
+    }
+}
